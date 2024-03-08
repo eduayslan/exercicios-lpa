7e394dbd6be6317ec20b31d61bd3986dd59bb290
eduayslan <eduardayslan10@gmail.com>
2024-03-08 10:29:16 -0300

    Exercicio 1

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('receber/nome', function (Request $request){
-    $name = $request -> input('name');
-    return $name;
-    });
-
-    Route::get('calcular/media', function(Request $request){
-        
-        $primeiraEntrada = $request -> input('primeiraNote');
-        $segundaEntrada = $request -> input('segundaNota');
-    
-        $media = ($primeiraEntrada + $segundaEntrada)/2;
-        return $media;
-    });
+Route::get('nome/usuario', function(Request $request){
+    $nomeUsuario = $request -> input('primeiroUsuario');
+    return $nomeUsuario;
+});
