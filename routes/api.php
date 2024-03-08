7ff116fdf0fcacca6905d7c568c6da3677c08b5b
eduayslan <161865859+eduayslan@users.noreply.github.com>
2024-03-08 10:29:34 -0300

    Delete routes/api.php

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('nome/usuario', function(Request $request){
-    $nomeUsuario = $request -> input('primeiroUsuario');
-    return $nomeUsuario;
-});
