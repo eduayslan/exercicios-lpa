b90d9d86a24700a491f9c99ba0387019b5e1d0e2
eduayslan <eduardayslan10@gmail.com>
2024-03-08 10:23:05 -0300

    Exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,12 @@ Route::get('receber/nome', function (Request $request){
     $name = $request -> input('name');
     return $name;
     });
+
+    Route::get('calcular/media', function(Request $request){
+        
+        $primeiraEntrada = $request -> input('primeiraNote');
+        $segundaEntrada = $request -> input('segundaNota');
+    
+        $media = ($primeiraEntrada + $segundaEntrada)/2;
+        return $media;
+    });
