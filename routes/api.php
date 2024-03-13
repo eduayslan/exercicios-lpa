385e1d21b0e3db7d00ca7fe18551a59b5633cff2
eduayslan <eduardoayslan10@gmail.com>
2024-03-13 10:17:31 -0300

    Exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ Route::get('receber/nome', function (Request $request){
         $media = ($primeiraEntrada + $segundaEntrada)/2;
         return $media;
     });
+    Route::get('senia', function(Request $request){
+        $nome = $request ->input('nome');
+        $idade = $request ->input('idade');
+    
+        return 'Meu nome é '. $nome . ', tenho ' . $idade . ' anos de idade';
+    });
